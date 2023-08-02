f04bc1b78ae6cfec969273932191f6db769e02ba
Fahd17 <fahdsattam8@gmail.com>
2023-08-02 18:45:37 +0300

    finished initial page

--- lib/views/initial_page.dart
+++ lib/views/initial_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -12,21 +12,7 @@ class InitialPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Color(0xffF9EFE5),
         elevation: 2,
-        title: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Image.asset("assets/logos/logo.png"),
-          SizedBox(
-            width: 8,
-          ),
-          Text(
-            "EasySave",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ]),
-      ),
+        title: TitleWidget()),
       body: SafeArea(
         child: Column(children: [
           Image.asset(
@@ -36,12 +22,78 @@ class InitialPage extends StatelessWidget {
           ),
           Expanded(
             child: Container(
+              width: MediaQuery.of(context).size.width,
               color: Colors.white,
-              
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(children: [
+                    Text("Easy Way Save", 
+                      style: TextStyle(
+                      fontSize: 25,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text("Make your payment experinece better today. No additional admin fee.",
+                      textAlign: TextAlign.center
+                    ),
+                  ]),
+                  Column(children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        fixedSize: Size(
+                          MediaQuery.of(context).size.width-50, 
+                          40
+                          )
+                      ),
+                      child: Text("Login"),
+                    ),
+                    ElevatedButton(onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                        side: BorderSide(color: Colors.black),
+                        fixedSize: Size(
+                          MediaQuery.of(context).size.width-50, 
+                          40
+                          )
+                      ),
+                     child: Text("Sign up")
+                     ),
+                  ]),
+                ],
+              )
             ),
           ),
         ]),
       ),
     );
+  }
+}
+
+class TitleWidget extends StatelessWidget {
+  const TitleWidget({super.key, this.title = "EasySave"});
+
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Image.asset("assets/logos/logo.png"),
+          SizedBox(
+            width: 8,
+          ),
+          Text(
+            title,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ]);
   }
 }
